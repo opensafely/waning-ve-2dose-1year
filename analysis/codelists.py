9f247161815b73e416bf7788dac37460095b94ae
Elsie Horne <31345323+elsie-h@users.noreply.github.com>
2022-06-08 08:49:27 +0100

    Update codelists.py

--- analysis/codelists.py
+++ analysis/codelists.py
@@ -253,10 +253,6 @@ eth2001_primis = codelist_from_csv(
     category_column="grouping_6_id",
 )
 
-###############################
-### COVID vaccination codes ###
-###############################
-
 #############################
 ### COVID infection codes ###
 #############################
@@ -290,46 +286,6 @@ covid_primary_care_probable_combined=combine_codelists(
     covid_primary_care_sequalae,
 )
 
-# suspected covid in primary care
-# covid_primary_care_suspected_covid_advice = codelist_from_csv(
-#     "codelists/opensafely-covid-identification-in-primary-care-suspected-covid-advice.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# covid_primary_care_suspected_covid_had_test = codelist_from_csv(
-#     "codelists/opensafely-covid-identification-in-primary-care-suspected-covid-had-test.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# covid_primary_care_suspected_covid_isolation_code = codelist_from_csv(
-#     "codelists/opensafely-covid-identification-in-primary-care-suspected-covid-isolation-code.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# covid_primary_care_suspected_covid_nonspecific_clinical_assessment = codelist_from_csv(
-#     "codelists/opensafely-covid-identification-in-primary-care-suspected-covid-nonspecific-clinical-assessment.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# covid_primary_care_suspected_covid_exposure = codelist_from_csv(
-#     "codelists/opensafely-covid-identification-in-primary-care-exposure-to-disease.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# primary_care_suspected_covid_combined = combine_codelists(
-#     covid_primary_care_suspected_covid_advice,
-#     covid_primary_care_suspected_covid_had_test,
-#     covid_primary_care_suspected_covid_isolation_code,
-#     covid_primary_care_suspected_covid_exposure,
-# )
-
-# COVID ICD10
-ICD10_I_codes = codelist_from_csv(
-    "codelists/opensafely-icd-10-chapter-i.csv",
-    system="icd10",
-    column="code",
-)
-
 # for identifying hospitalisations from emergency data
 covid_emergency = codelist(
     ["1240751000000100"],
@@ -341,147 +297,7 @@ discharged_to_hospital = codelist(
     system="snomed",
 )
 
-###########################
-### clinical covariates ###
-###########################
-# Note that I have used differnt codelists to define clinical comorbidities 
-# compared to those used to define the JCVI groupings. This is because I used 
-# different repos as templates. May resolve at some point, but OK for now.
-# chronic_cardiac_disease_codes = codelist_from_csv(
-#     "codelists/opensafely-chronic-cardiac-disease.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# heart_failure_codes = codelist_from_csv(
-#     "codelists/opensafely-heart-failure.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# other_heart_disease_codes = codelist_from_csv(
-#     "codelists/opensafely-other-heart-disease.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# diabetes_codes = codelist_from_csv(
-#     "codelists/opensafely-diabetes.csv", 
-#     system="ctv3", 
-#     column="CTV3ID"
-# )
-# dialysis_codes = codelist_from_csv(
-#     "codelists/opensafely-dialysis.csv",
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# chronic_liver_disease_codes = codelist_from_csv(
-#     "codelists/opensafely-chronic-liver-disease.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# current_copd_codes = codelist_from_csv(
-#     "codelists/opensafely-current-copd.csv",
-#     system="ctv3", 
-#     column="CTV3ID"
-# )
-# learning_disability_codes = codelist_from_csv(
-#     "codelists/opensafely-learning-disabilities.csv",
-#     system="ctv3",
-#     column="CTV3Code",
-# )
-# downs_syndrome_codes = codelist_from_csv(
-#     "codelists/opensafely-down-syndrome.csv",
-#     system="ctv3",
-#     column="code",
-# )
-# cerebral_palsy_codes = codelist_from_csv(
-#     "codelists/opensafely-cerebral-palsy.csv",
-#     system="ctv3",
-#     column="code",
-# )
-# learning_disability_including_downs_syndrome_and_cerebral_palsy_codes=combine_codelists(
-#     learning_disability_codes,
-#     downs_syndrome_codes,
-#     cerebral_palsy_codes,
-# )
-# cystic_fibrosis_codes = codelist_from_csv(
-#     "codelists/opensafely-cystic-fibrosis.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# other_respiratory_conditions_codes = codelist_from_csv(
-#     "codelists/opensafely-other-respiratory-conditions.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# lung_cancer_codes = codelist_from_csv(
-#     "codelists/opensafely-lung-cancer.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# haematological_cancer_codes = codelist_from_csv(
-#     "codelists/opensafely-haematological-cancer.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# cancer_excluding_lung_and_haematological_codes = codelist_from_csv(
-#     "codelists/opensafely-cancer-excluding-lung-and-haematological.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# chemotherapy_or_radiotherapy_codes = codelist_from_csv(
-#     "codelists/opensafely-chemotherapy-or-radiotherapy.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# solid_organ_transplantation_codes = codelist_from_csv(
-#     "codelists/opensafely-solid-organ-transplantation.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# bone_marrow_transplant_codes = codelist_from_csv(
-#     "codelists/opensafely-bone-marrow-transplant.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# sickle_cell_disease_codes = codelist_from_csv(
-#     "codelists/opensafely-sickle-cell-disease.csv", 
-#     system="ctv3", 
-#     column="CTV3ID",
-# )
-# permanent_immunosuppression_codes = codelist_from_csv(
-#     "codelists/opensafely-permanent-immunosuppression.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# temporary_immunosuppression_codes = codelist_from_csv(
-#     "codelists/opensafely-temporary-immunosuppression.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# asplenia_codes = codelist_from_csv(
-#     "codelists/opensafely-asplenia.csv", 
-#     system="ctv3", 
-#     column="CTV3ID"
-# )
-# dmards_codes = codelist_from_csv(
-#     "codelists/opensafely-dmards.csv", 
-#     system="snomed", 
-#     column="snomed_id",
-# )
-# dementia_codes = codelist_from_csv(
-#     "codelists/opensafely-dementia-complete.csv", 
-#     system="ctv3", 
-#     column="code"
-# )
-# other_neuro_codes = codelist_from_csv(
-#     "codelists/opensafely-other-neurological-conditions.csv",
-#     system="ctv3",
-#     column="CTV3ID",
-# )
-# psychosis_schizophrenia_bipolar_affective_disease_codes = codelist_from_csv(
-#     "codelists/opensafely-psychosis-schizophrenia-bipolar-affective-disease.csv",
-#     system="ctv3",
-#     column="CTV3Code",
-# )
+
 flu_med_codes = codelist_from_csv(
     "codelists/opensafely-influenza-vaccination.csv",  
     system="snomed",  
